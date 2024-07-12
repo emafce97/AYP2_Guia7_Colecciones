f6876b0457387639a057c2527a0f9f6e9c2c739d
Emanuel Faillace <102189125+emafce97@users.noreply.github.com>
2024-07-12 18:41:57 -0300

    Delete src directory

--- src/excepciones/PrioridadEx.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class PrioridadEx extends Exception {
-
-    public PrioridadEx(){
-        super("El valor se encuentra fuera de los limites!");
-    }
-
-}
--- src/excepciones/TiempoEjecucionEx.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class TiempoEjecucionEx extends Exception{
-
-    public TiempoEjecucionEx(){
-        super("El tiempo de ejecucion es incorrecto!");
-    }
-
-}
